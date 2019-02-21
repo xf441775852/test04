a8e5c749ccc8025ab153cc31e5ad8d8897bef4e9
熊锋 <441775852@qq.com>
2019-02-21 20:12:36 +0800

    把本地工程添加到本地仓库中

--- git_test/src/com/company/git/TestGit.java
+++ git_test/src/com/company/git/TestGit.java
@@ -4,4 +4,8 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello,git");
     }
+
+    public void test(){
+        System.out.println("修改");
+    }
 }
