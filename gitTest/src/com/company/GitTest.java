4c73d804533d4fd7e9b6d175cee107f69279c9f7
熊锋 <441775852@qq.com>
2019-05-03 22:55:00 +0800

    创建GiTTest

--- gitTest/src/com/company/GitTest.java
+++ gitTest/src/com/company/GitTest.java
@@ -4,4 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("提交到git");
     }
+    public void test(){
+        System.out.println("修改");
+    }
 }
